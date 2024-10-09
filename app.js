b3d2cdf79180d42e545451131b6e75680705bf81
lipstick-turtleback <857949+lipstick-turtleback@users.noreply.github.com>
2024-10-09 17:23:56 +0300

    many improvements

--- app.js
+++ app.js
@@ -1,85 +1,92 @@
 function getApp() {
-  let parsedDictionary = [];
+  let dictionaryItems = [];
 
-  const fileInput = document.getElementById("csvFileInput");
-  const searchResult = document.getElementById("searchResult");
-  const searchInput = document.getElementById("searchInput");
+  const fileInputEl = document.getElementById("csvFileInput");
+  const searchResultEl = document.getElementById("searchResult");
+  const searchInputEl = document.getElementById("searchInput");
 
-  const loadCSV = () => {
-    console.log("loadCSV start");
-
-    if (fileInput.files.length === 0) {
+  const onReadCsvFileClick = () => {
+    if (fileInputEl.files.length === 0) {
       alert("Please select a CSV file first.");
       return;
     }
 
-    const file = fileInput.files[0];
-    const reader = new FileReader();
+    const firstSelectedFile = fileInputEl.files[0];
+    const fileReader = new FileReader();
 
-    reader.onload = function (event) {
-      const csvData = event.target.result;
-      parsedDictionary = parseCSV(csvData);
-      searchResult.textContent = "";
+    fileReader.onload = function (event) {
+      const csvFileContent = event.target.result;
+      dictionaryItems = parseFullCSVContent(csvFileContent);
+      searchResultEl.textContent = "";
     };
 
-    reader.readAsText(file);
-
-    console.log("loadCSV end");
+    fileReader.readAsText(firstSelectedFile);
   };
 
-  const parseCSV = (data) => {
-    console.log("parse csv start");
+  const parseFullCSVContent = (data = "") => {
+    let result = [];
 
-    const rows = data.trim().split("\n");
-    const dictionary = [];
+    let c1 = data.split(",").length;
+    let c2 = data.split(";").length;
 
-    rows.forEach((row) => {
-      const [key, value] = row.split(",").map((item) => item.trim());
-      dictionary.push({ key, value, searchString: `${key} ${value}` });
+    let itemsSeparator = c1 > c2 ? "," : ";";
+    const lineSeparator = "\n";
+
+    const rowsStrings = data.split(lineSeparator);
+
+    rowsStrings.forEach((row) => {
+      const lineItems = row.split(itemsSeparator);
+      if (lineItems.length > 1) {
+        const key = lineItems[0].trim();
+        const value = lineItems[1].trim();
+        result.push({ key, value, searchString: `${key} ${value}` });
+      }
     });
 
-    console.log("parse csv end");
+    alert(`${result.length} records parsed!`);
 
-    alert(`${dictionary.length} records parsed!`);
-
-    return dictionary;
+    return result;
   };
 
-  const searchWord = () => {
-    let searchString = searchInput.value.trim().toLowerCase();
+  const getSearchString = () => {
+    let searchString = searchInputEl.value?.trim().toLowerCase();
+    return searchString;
+  };
 
-    if (parsedDictionary.length <= 0) {
-      searchResult.textContent = "Please load csv file first.";
+  const searchTranslations = () => {
+    let searchString = getSearchString();
+
+    if (dictionaryItems.length <= 0) {
+      searchResultEl.textContent = "Please load csv file first.";
       return;
     }
 
     if (searchString === "") {
-      searchResult.textContent = "Please enter a word to search.";
+      searchResultEl.textContent = "Please enter a word to search.";
       return;
     }
 
     if (searchString.length < 2) {
-      searchResult.textContent = "Please enter at least 3 chars.";
+      searchResultEl.textContent = "Please enter at least 3 chars.";
       return;
     }
 
-    const translations = parsedDictionary.filter(
+    const translations = dictionaryItems.filter(
       (x) => x.searchString.indexOf(searchString) >= 0
     );
 
-    if (translations.length < 29) {
+    if (translations.length < 49) {
       let translationsString = translations
         .map((x) => `${x.key} => ${x.value}`)
         .join("\n");
 
-      searchResult.textContent = `${translationsString}`;
+      searchResultEl.textContent = `${translationsString}`;
     }
   };
 
   const result = {
-    loadCSV,
-    parseCSV,
-    searchWord,
+    onReadCsvFileClick,
+    searchTranslations,
   };
 
   return result;
